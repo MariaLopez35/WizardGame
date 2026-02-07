ed84783a3b2ad8acdf0eb0fbac2dd343f24f4d90
María López <marialopezdaw@gmail.com>
2026-02-07 18:24:27 +0100

    created Wizard – Step 1: Available Time

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import "./App.css";
 import logo from "./assets/logowizard.png";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 
 function Header() {
   return (
@@ -9,27 +11,86 @@ function Header() {
   );
 }
 
-function HeroSection() {
+function HeroSection({ handleStart }) {
   return (
-    <div className="hero">
-      <div class="hero-content">
-        <h1 className="hero-title">¡Listo para jugar!</h1>
-        <p className="hero-eslogan">Encuentra el juego para ti al instante</p>
-        <button className="hero-button">
-          <i className="fas fa-gamepad"></i>Encontrar un juego
-        </button>
+    <>
+    
+      <div className="hero">
+        <div className="hero-content">
+          <h1 className="hero-title">¡Listo para jugar!</h1>
+          <p className="hero-eslogan">Encuentra el juego para ti al instante</p>
+          <Link to="wizard">
+            <button onClick={handleStart} className="hero-button">
+              <i className="fas fa-gamepad"></i>Encontrar un juego
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
-
-function App() {
+function TimeAvalaible() {
   return (
     <>
-      <Header />
-      <HeroSection />
+      <section className="wizard-step">
+        <h2 className="step-title">PASO 1</h2>
+        <h1 className="step-subtitle">Tiempo disponible</h1>
+        <p className="step-question">¿Cuánto tiempo tienes para jugar?</p>
+
+        <div className="options-grid">
+          <button className="time-card">
+            <i className="fa-solid fa-clock icons"></i>
+            15 min
+          </button>
+
+          <button className="time-card">
+            <i className="fa-solid fa-clock icons"></i>
+            30 min
+          </button>
+
+          <button className="time-card">
+            <i className="fa-solid fa-clock icons"></i>1 hora
+          </button>
+
+          <button className="time-card">
+            <i className="fa-solid fa-clock icons"></i>
+            Más de 2 horas
+          </button>
+        </div>
+        <Link to="/mental-state">
+          <button className="next-btn">Siguiente</button>
+        </Link>
+      </section>
     </>
+  );
+}
+
+function App() {
+  const [pulsed, setPulsed] = useState(true);
+
+  const handleStart = () => {
+    setPulsed(false);
+  };
+
+  return (
+    <>
+    <Header />
+    <Routes>
+      <Route
+        path="/"
+        element={
+          pulsed ? (
+            <HeroSection handleStart={handleStart} />
+          ) : (
+            <Navigate to="/time-available" />
+          )
+        }
+      />
+      <Route path="/time-available" element={<TimeAvalaible />} />
+    </Routes>
+    </>
+    
   );
 }
 
