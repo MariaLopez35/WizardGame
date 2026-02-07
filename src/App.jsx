ead5b7e241cc174b683d4f06aafcd06de0c09ad9
María López <marialopezdaw@gmail.com>
2026-02-07 12:47:59 +0100

    Created the hero section with a welcome message and a button

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,26 @@ function Header() {
   );
 }
 
+function HeroSection() {
+  return (
+    <div className="hero">
+      <div class="hero-content">
+        <h1 className="hero-title">¡Listo para jugar!</h1>
+        <p className="hero-eslogan">Encuentra el juego para ti al instante</p>
+        <button className="hero-button">
+          <i className="fas fa-gamepad"></i>Encontrar un juego
+        </button>
+      </div>
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   );
 }
