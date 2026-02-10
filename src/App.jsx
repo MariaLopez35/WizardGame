89fefde2c249b00880f22f5c0c2d0fa23b36276f
María López <marialopezdaw@gmail.com>
2026-02-10 17:39:42 +0100

    add step two: mental-energy

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function Header() {
 function HeroSection({ handleStart }) {
   return (
     <>
-    
       <div className="hero">
         <div className="hero-content">
           <h1 className="hero-title">¡Listo para jugar!</h1>
@@ -58,13 +57,49 @@ function TimeAvalaible() {
             Más de 2 horas
           </button>
         </div>
-        <Link to="/mental-state">
+        <Link to="/mental-energy">
           <button className="next-btn">Siguiente</button>
         </Link>
       </section>
     </>
   );
 }
+
+function MentalEnergy() {
+  return (
+    <section className="wizard-step">
+      <h2 className="step-title">PASO 2</h2>
+      <h1 className="step-subtitle">Energía mental</h1>
+      <p className="step-question">¿Cómo está tu energía mental ahora?</p>
+
+      <div className="options-grid">
+        <button className="time-card">
+          <i className="fa-solid fa-spa icons"></i>
+          Relajado
+        </button>
+
+        <button className="time-card">
+          <i className="fa-regular fa-face-smile icons"></i>
+          Normal
+        </button>
+
+        <button className="time-card">
+          <i className="fa-solid fa-bolt icons"></i>
+          Con energía
+        </button>
+
+        <button className="time-card">
+          <i className="fa-solid fa-fire icons"></i>A tope
+        </button>
+      </div>
+      <Link to="/game-type">
+        <button className="next-btn">Siguiente</button>
+      </Link>
+    </section>
+  );
+}
+
+
 
 function App() {
   const [pulsed, setPulsed] = useState(true);
@@ -75,22 +110,23 @@ function App() {
 
   return (
     <>
-    <Header />
-    <Routes>
-      <Route
-        path="/"
-        element={
-          pulsed ? (
-            <HeroSection handleStart={handleStart} />
-          ) : (
-            <Navigate to="/time-available" />
-          )
-        }
-      />
-      <Route path="/time-available" element={<TimeAvalaible />} />
-    </Routes>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            pulsed ? (
+              <HeroSection handleStart={handleStart} />
+            ) : (
+              <Navigate to="/time-available" />
+            )
+          }
+        />
+        <Route path="/time-available" element={<TimeAvalaible />} />
+        <Route path="/mental-energy" element={<MentalEnergy />} />
+        
+      </Routes>
     </>
-    
   );
 }
 
